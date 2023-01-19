05794f4e73b833a3533c333272c79fe4df98fed1
Zain Jaafar <zainjaafar120@gmail.com>
2023-01-19 20:26:34 +1300

    added flags

--- settings.py
+++ settings.py
@@ -5,6 +5,9 @@ FPS = 60
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
+
+FLAGS = pygame.DOUBLEBUF | pygame.HWSURFACE
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Typing Game")
 
 FONT = pygame.font.Font("Fonts/Pixeltype.ttf", 32)
