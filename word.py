0301ab5dad88fa21063fd536cb99e7ef181c37d1
Zain Jaafar <zainjaafar120@gmail.com>
2023-01-30 22:39:40 +1300

    reworked scale system

--- word.py
+++ word.py
@@ -1,13 +1,14 @@
 from pygame import sprite, Surface, Rect
 
 class Word(sprite.Sprite):
-    def __init__(self, font, colour, word, position, scale):
+    def __init__(self, font, colour, word, position):
         super().__init__()
         self.word = word
         self.text_surf = font.render(self.word, False, colour)
-        self.image = Surface(scale).convert()
+        self.scale = (self.text_surf.get_width() + 10, 32)
+        self.image = Surface(self.scale).convert()
         self.image.set_colorkey((0, 0, 0))
-        self.rect = Rect(position, scale)
+        self.rect = Rect(position, self.scale)
         
         self.image.blit(self.text_surf, (10, 10))
     
