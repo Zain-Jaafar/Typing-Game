d7f1790b0558a61d565e8f36632444684f6752b1
Zain Jaafar <zainjaafar120@gmail.com>
2023-01-31 23:19:32 +1300

    added ability to change size of font, defaults to 32

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
 
 FLAGS = pygame.DOUBLEBUF | pygame.HWSURFACE
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), FLAGS)
 pygame.display.set_caption('Typing Game')
 
 FONT = pygame.font.Font('Fonts/Pixeltype.ttf', 32)
@@ -31,7 +31,8 @@ game_states = {
     "Game Lose": False
 }
 
-def draw_text(font, colour, text, position):
+def draw_text(font, colour, text, position, text_size=32):
+    font = pygame.font.Font('Fonts/Pixeltype.ttf', text_size)
     text_surf = font.render(text, False, colour)
     text_rect = pygame.Rect((0, 0), (text_surf.get_size()))
     text_rect.center = position
